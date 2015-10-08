84e95995e2443b4c9a801404fb783de0d7d81263
Lokesh Cherukuri <lokesh.cherukuri@yahoo.com>
2015-10-08 03:54:50 -0400

    Bugfix with ajax requests

--- src/com/tracker/config/security/AccessDenyHandler.java
+++ src/com/tracker/config/security/AccessDenyHandler.java
@@ -14,12 +14,15 @@ public class AccessDenyHandler implements AccessDeniedHandler{
 	@Override
 	public void handle(HttpServletRequest request, HttpServletResponse response,
 			AccessDeniedException arg2) throws IOException, ServletException {
-			
-		String referer=request.getHeader("referer");
-		if(referer!=null)
-			response.sendRedirect(referer);
-		else
-			response.sendRedirect(request.getContextPath()+"/accessdenied");
+		if("XMLHttpRequest".equals(request.getHeader("X-Requested-With")))
+			response.sendError(HttpServletResponse.SC_FORBIDDEN);
+		else {
+			String referer=request.getHeader("referer");
+			if(referer!=null)
+				response.sendRedirect(referer);
+			else
+				response.sendRedirect(request.getContextPath()+"/accessdenied");
+		}
 	}
 
 }
--- src/com/tracker/web/controllers/MainController.java
+++ src/com/tracker/web/controllers/MainController.java
@@ -1,34 +1,16 @@
 package com.tracker.web.controllers;
 
-import java.security.Principal;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.tracker.web.service.implementations.UserServiceImpl.CustomUser;
-import com.tracker.web.service.interfaces.UserService;
-
 @Controller
 public class MainController {
 
-	private UserService userService;
-
-	@Autowired
-	public void setUserService(UserService userService) {
-		this.userService = userService;
-	}
-
 	@RequestMapping(value={"/","index"},method=RequestMethod.GET)
-	public String showIndexPage(Principal principal,Model model)
+	public String showIndexPage(Model model)
 	{
-		if(principal!=null)
-		{
-			CustomUser customUser=(CustomUser) userService.loadUserByUsername(principal.getName());
-			model.addAttribute("loggeduser", customUser);
-		}
 		return "pages/index";
 	}
 	
